8a120eb58b293da58b87861deaaecca2ef8f6466
Belvix <16433060+Belvix@users.noreply.github.com>
2023-09-13 17:46:24 +0530

    Basic movement animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,15 @@ public class PlayerMovement : MonoBehaviour
     float vertical;
     public float Speed;
     Rigidbody2D rb;
+    SpriteRenderer sprite;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,6 +26,37 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        Debug.Log(rb.velocity);
+        if (rb.velocity.y < 0.0f)
+        {
+            anim.SetBool("walking_down", true);
+        }
+        else if (rb.velocity.y > 0.0f)
+        {
+            anim.SetBool("walking_down", false);
+            anim.SetBool("walking_up", true);
+        }
+        else if (rb.velocity.x > 0.0f)
+        {
+            anim.SetBool("walking_right", true);
+            sprite.flipX = false;
+        }
+        else if (rb.velocity.x < 0.0f)
+        {
+            anim.SetBool("walking_right", true);
+            sprite.flipX = true;
+
+        }
+        if (rb.velocity.x == 0.0f)
+        {
+            anim.SetBool("walking_right", false);
+        }
+        if (rb.velocity.y == 0.0f)
+        {
+            Debug.Log("reset reached");
+            anim.SetBool("walking_down", false);
+            anim.SetBool("walking_up", false);
+        }
     }
     private void FixedUpdate()
     {
